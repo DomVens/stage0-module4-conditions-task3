73e749f4045d84244a116634365e03457ecb5c99
Domantas Venslovas <domantas.venslovas@stud.vilniustech.lt>
2022-08-29 21:34:07 +0300

    Done?

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character<32 && character>12) System.out.println("wrong alphabet!");
+        else if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' ) System.out.println(character + " is vowel");
+        else System.out.println(character + " is consonant");
     }
 }
